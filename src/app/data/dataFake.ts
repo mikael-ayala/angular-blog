048ac7fffb3ab53ddbe4aef14a46967cc72cecb3
mikael-ayala <mikael.ayala.santos@gmail.com>
2023-10-15 12:01:59 -0300

    add cards content

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,24 +3,24 @@ export const dataFake = [
     id: '1',
     "photo": "https://i.ytimg.com/vi/cc4rcnC5ECM/maxresdefault.jpg",
     "title": "Nova Expansão de Guild Wars 2 já está no ar!",
-    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book"
+    "description": "Embarque em uma aventura épica com a quarta expansão do MMORPG aclamado pela crítica, Guild Wars 2: Secrets of the Obscure. Em um mundo finalmente protegido dos Elder Dragons, novas ameaças surgem em busca de poder. Descubra os mistérios da Torre do Mago e defenda os céus contra as incursões de Mists. Guild Wars 2: Secrets of the Obscure Standard Edition inclui: um ano de história da expansão e atualizações de conteúdo, um slot de Inventário Compartilhado, um boost de nível máximo."
   },
   {
     id: 2,
     "photo": "https://sm.ign.com/t/ign_br/screenshot/default/images_y2jf.320.jpg",
     "title": "Marvel divulga trailer da timeline oficial de seus filmes e séries",
-    "description": "blabla"
+    "description": "Com 32 filmes, algumas séries e muito mais por vir, memorizar a timeline completa do MCU é algo muito díficil e até um poder que poucos no mundo tem. Mas, esses dias de dúvidas poderão acabar. Divulgado nas redes sociais, a Marvel publicou um trailer sobre a linha do tempo completa do MCU, explicando não somente a ordem se suas produções, mas também personagens, histórias e curiosidades para os amantes de quadrinhos e das adaptações. Intitulado Marvel Studios: The Marvel Cinematic Universe: An Official Timeline, o livro será uma espécie de almanaque para que fãs consigam entender a tão complexa linha do tempo da Marvel, que ficou ainda mais difícil após os eventos da primeira temporada de Loki e as consequências do filme de Doutor Estranho: Multiverso da Loucura."
   },
   {
     id: 3,
     "photo": "https://cdn.ome.lt/PfG0BSMaaOItVqALfoXpRRpe9hc=/987x0/smart/uploads/conteudo/fotos/ahsoka-audiencia-estreia.jpg",
     "title": "Ahsoka | Estreia tem audiência abaixo das de Obi-Wan Kenobi e The Mandalorian",
-    "description": "bla"
+    "description": "Nova série do universo Star Wars, Ahsoka infelizmente não conseguiu atrair a mesma quantidade de espectadores de outras produções da franquia criadas para o Disney+. De acordo com dados divulgados pela Samba TV, o primeiro episódio do programa foi assistido por cerca de 1,2 milhão de pessoas. Os números são similares aos da estreia de Andor, mas bem abaixo do registrado nas estreias de Obi-Wan Kenobi (2,2 milhões) e da terceira temporada de The Mandalorian (1,7 milhão) (via Deadline). Ainda de acordo com a Samba TV, houve também uma queda no número de pessoas que assistiram ao segundo episódio, com 956 mil pessoas dando sequência a Ahsoka após a estreia."
   },
   {
     id: 4,
-    "photo": "https://gameplayscassi.com.br/wp-content/uploads/2021/10/geforce-now-1-1-696x392.jpg",
-    "title": "GeForce Now encerra operações na Rússia",
-    "description": "blu"
+    "photo": "https://s2-techtudo.glbimg.com/sguGDJvo9OJ5cz_Zs1EFGq0HTyY=/0x0:695x390/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/j/c/fUHqL4TYKKT4Ns5EIm3Q/activision-blizzard-microsoft-compra-call-of-duty-diablo-world-warcraft.jpg",
+    "title": "Microsoft conclui a compra da Activision Blizzard",
+    "description": "A Microsoft concluiu oficialmente a aquisição da Activision Blizzard após um processo burocrático mundial que demorou quase 2 anos. A fabricante do Xbox Series X e Xbox Series S anunciou sua intenção de adquirir a produtora de games como Call of Duty: Modern Warfare 3 e Diablo 4 em janeiro de 2022 por US$ 68,7 bilhões, em torno de R$ 346 bilhões na cotação atual. Esse montante representa o maior negócio já registrado na história da indústria dos games. A aquisição foi concluída nesta sexta-feira, 13, como já indicavam rumores, horas após o último entrave ter sido resolvido com o órgão CMA (Autoridade de Competição e Mercados) do Reino Unido, que deu um parecer positivo para a compra."
   }
 ]
